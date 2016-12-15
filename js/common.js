df730bb3fbb6de788860c0f93662334bc9040455
Richard Großer <richard.grosser@thulb.uni-jena.de>
2016-12-15 08:57:00 +0100

    Calculate overly div size after facet clicking, to avoid problems with browser resize after page loading (refs #89)

--- js/common.js
+++ js/common.js
@@ -290,22 +290,23 @@ function keyboardShortcuts() {
  * Setup facets
  */
 function setupFacets() {
-  $("#overlay").css({
-    opacity : 0.5,
-    left    : 20,
-    width   : $("#resultlist").outerWidth() + 25,
-    height  : $("#resultlist").outerHeight()
-  });
-
-  $("#img-load").css({
-    top  : ($("#resultlist").height() / 10),
-    left : ($("#resultlist").width() / 2)
-  });
-    
   // Advanced facets
   $('.facetAND,.facetOR').click(function facetBlocking() {
     $("#resultlist").css('pointer-events', 'none');
     $("#resultlist").css('opacity', '0.5');
+    
+    $("#overlay").css({
+      opacity : 0.5,
+      left    : 20,
+      width   : $("#resultlist").outerWidth() + 25,
+      height  : $("#resultlist").outerHeight() + 5
+    });
+
+    $("#img-load").css({
+      top   : ($("#resultlist").height() / 10),
+      left  : ($("#resultlist").width() / 2)
+    });
+    
     $("#overlay").fadeIn();
     
     $(this).closest('.collapse').html('<div class="list-group-item">' + VuFind.translate('loading') + '...</div>');
