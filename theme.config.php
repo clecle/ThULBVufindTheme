38019bcd7b1b28d89d9c862152da1e4981868118
shkgrto <tobiasgroth@gmx.de>
2019-06-05 14:01:28 +0200

    merged master into branch

--- theme.config.php
+++ theme.config.php
@@ -7,16 +7,18 @@ return array(
             'ThULB\View\Helper\Root\RecordLink' => 'ThULB\View\Helper\Root\Factory::getRecordLink',
             'ThULB\View\Helper\Root\Record' => 'ThULB\View\Helper\Root\Factory::getRecord',
             'ThULB\View\Helper\Root\ServerType' => 'Zend\ServiceManager\Factory\InvokableFactory',
+            'ThULB\View\Helper\Root\Session' => 'ThULB\View\Helper\Root\Factory::getSession',
         ],
         'aliases' => array (
             'record' => 'ThULB\View\Helper\Root\Record',
             'recordlink' => 'ThULB\View\Helper\Root\RecordLink',
             'thulb_serverType' => 'ThULB\View\Helper\Root\ServerType',
+            'thulb_session' => 'ThULB\View\Helper\Root\Session',
         ),
     ],
     'favicon' => 'thulb_favicon.ico',
     'js' => array(
-            'thulb.js',
-            'jquery-ui.min.js',
-        ),
+        'thulb.js',
+        'jquery-ui.min.js',
+    ),
 );
