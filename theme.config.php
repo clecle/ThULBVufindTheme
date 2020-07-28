cddf94c9724c7f7b6fc1d0c411a5ad974de0f814
Tobias Groth <tobiasgroth@uni-jena.de>
2020-07-28 18:10:29 +0200

    Replaced zend usage with laminas.

--- theme.config.php
+++ theme.config.php
@@ -6,9 +6,9 @@ return array(
             'VuFind\View\Helper\Root\RecordDataFormatter' => 'ThULB\View\Helper\Root\RecordDataFormatterFactory',
             'ThULB\View\Helper\Root\RecordLink' => 'ThULB\View\Helper\Root\Factory::getRecordLink',
             'ThULB\View\Helper\Root\Record' => 'ThULB\View\Helper\Root\Factory::getRecord',
-            'ThULB\View\Helper\Root\ServerType' => 'Zend\ServiceManager\Factory\InvokableFactory',
+            'ThULB\View\Helper\Root\ServerType' => 'Laminas\ServiceManager\Factory\InvokableFactory',
             'ThULB\View\Helper\Root\Session' => 'ThULB\View\Helper\Root\Factory::getSession',
-            'ThULB\View\Helper\Root\RemoveThBibFilter' => 'Zend\ServiceManager\Factory\InvokableFactory',
+            'ThULB\View\Helper\Root\RemoveThBibFilter' => 'Laminas\ServiceManager\Factory\InvokableFactory',
             \ThULB\View\Helper\Root\DoiLinker::class => 'ThULB\View\Helper\Root\Factory::getDoiLinker',
         ],
         'aliases' => array (
