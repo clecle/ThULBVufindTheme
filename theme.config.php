d0cfa265710c031f731a4837713ec555a9960974
Tobias Groth <tobias.groth@uni-jena.de>
2021-02-16 15:05:57 +0100

    Removed 'Thuringian bibliography' checkbox and always show 'Thuringian bibliography' facet. Added a 'more' button if 'Thuringian bibliography' has more than 6 parent facets. refs #395

--- theme.config.php
+++ theme.config.php
@@ -9,7 +9,6 @@ return array(
             'ThULB\View\Helper\Root\Record' => 'ThULB\View\Helper\Root\Factory::getRecord',
             'ThULB\View\Helper\Root\ServerType' => 'Laminas\ServiceManager\Factory\InvokableFactory',
             'ThULB\View\Helper\Root\Session' => 'ThULB\View\Helper\Root\Factory::getSession',
-            'ThULB\View\Helper\Root\RemoveThBibFilter' => 'Laminas\ServiceManager\Factory\InvokableFactory',
             \ThULB\View\Helper\Root\DoiLinker::class => 'ThULB\View\Helper\Root\Factory::getDoiLinker',
         ],
         'aliases' => array (
@@ -18,7 +17,6 @@ return array(
             'recordLink' => 'ThULB\View\Helper\Root\RecordLink',
             'thulb_serverType' => 'ThULB\View\Helper\Root\ServerType',
             'thulb_session' => 'ThULB\View\Helper\Root\Session',
-            'thulb_removeThBibFilter' => 'ThULB\View\Helper\Root\RemoveThBibFilter',
             'thulb_doiLinker' => \ThULB\View\Helper\Root\DoiLinker::class,
         ),
     ],
